16c474dd8556a16a3ac03f204a03ba3b8e149b54
erha <fikri41153.pesonaxii@gmail.com>
2023-03-14 19:52:18 +0700

    update migrations

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +17,18 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        // schedule every 1 month to update total_masuk from absensi table
+        $schedule->call(function () {
+            $users = DB::table('users')->get();
+            foreach ($users as $user) {          
+                //mengambil data dari tanngal 1 sampai akhir bulan di tahun sekarang
+                $date = date('Y-m-d', strtotime('first day of this month'));
+                $date2 = date('Y-m-d', strtotime('last day of this month'));
+                // mengambil jumlah data dari tanggal 1 sampai akhir bulan di tahun sekarang
+                $total_masuk = DB::table('absensi')->where('id_karyawan', $user->id_karyawan)->whereBetween('waktu', [$date, $date2])->count();
+                DB::table('gaji')->where('id_karyawan', $user->id_karyawan)->update(['total_masuk' => $total_masuk]);
+            }
+        })->monthly();
         // $schedule->command('inspire')->hourly();
     }
 
--- database/migrations/2023_03_13_032048_create_absensi_table.php
+++ database/migrations/2023_03_13_032048_create_absensi_table.php
@@ -15,9 +15,13 @@ class CreateAbsensiTable extends Migration
     {
         Schema::create('absensi', function (Blueprint $table) {
             $table->id();
-            $table->string('id_karyawan');
+            $table->bigInteger('id_karyawan')->unsigned();
             $table->dateTimeTz('waktu', $precision = 0);
             $table->timestamps();
+        });
+        
+        Schema::table('absensi', function (Blueprint $table) {
+            $table->foreign('id_karyawan')->references('id_karyawan')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
