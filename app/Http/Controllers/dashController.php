24b9b40b85524021933d391043573fe5fca3b47c
erha <fikri41153.pesonaxii@gmail.com>
2023-03-15 22:22:10 +0700

    update proses login

---
+++ app/Http/Controllers/dashController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\jabatan;
+use App\Models\agama;
+use App\Models\User;
+class dashController extends Controller
+{
+    //
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,21 +3,30 @@
 @section('content')
 <h1 class="auth-title">Log in.</h1>
 <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
-<form action="index.html">
+@if (\Session::has('message'))
+<div class="alert alert-danger">    
+    <ul>
+        <li>{!! \Session::get('message') !!}</li>
+        
+    </ul>
+</div>
+@endif
+<form action="/prosesLogin" method="post">
+    @csrf
     <div class="form-group position-relative has-icon-left mb-4">
-        <input type="text" class="form-control form-control-xl" placeholder="Username">
+        <input type="text" class="form-control form-control-xl" name="user" placeholder="NIK Or Email Or No Phone">
         <div class="form-control-icon">
             <i class="bi bi-person"></i>
         </div>
     </div>
-    <div class="form-group position-relative has-icon-left mb-4">
-        <input type="password" class="form-control form-control-xl" placeholder="Password">
+    <div class="form-group position-relative has-icon-left mb-4"> 
+        <input type="password" class="form-control form-control-xl" name="password" placeholder="Password">
         <div class="form-control-icon">
             <i class="bi bi-shield-lock"></i>
         </div>
     </div>
     <div class="form-check form-check-lg d-flex align-items-end">
-        <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+        <input class="form-check-input me-2" type="checkbox"  id="flexCheckDefault">
         <label class="form-check-label text-gray-600" for="flexCheckDefault">
             Keep me logged in
         </label>
