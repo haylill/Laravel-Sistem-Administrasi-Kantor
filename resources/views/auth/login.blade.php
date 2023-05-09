73ac024c12adb4bd30d3554a87518c2079143c10
erha <fikri41153.pesonaxii@gmail.com>
2023-05-09 14:31:20 +0700

    Disuruh commit sama git

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,6 @@
             <i class="bi bi-shield-lock"></i>
         </div>
     </div>
-    
     <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
 </form>
 <div class="text-center mt-5 text-lg fs-4">    
